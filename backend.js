e9dd31d7a3ba1c9a3356f5523d189a7f4a8aa195
Evan Misshula <emisshula@jjay.cuny.edu>
2022-01-20 15:36:21 -0500

    working copy

--- backend.js
+++ backend.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const pool = require('./db.js'); //database connection pooling
+const cors = require("cors");
+const pool = require("./db.js"); //database connection pooling
 
 //middleware
 app.use(cors());
@@ -9,35 +9,39 @@ app.use(express.json()); //access to json body, client side data
 
 //ROUTES
 //get all underCustody data
-app.get("/underCustody", async(req,res) => {
-    try {
-      // console.log(req. query)
-      const allData = await pool.query(" \
+app.get("/underCustody", async (req, res) => {
+  try {
+    // console.log(req. query)
+    const allData = await pool.query(
+      " \
         SELECT sex, age, modEthRace, timeServed, \
                timeServedBinned, ageBinned, crimeCounty, \
                downstateResident, nycResident, \
                prisonSecLevel, prison \
         FROM underCustody \
-      ");
-      res.json(allData.rows);
-    } catch (err) {
-      console.error(err.message);
-    }
-})
+      "
+    );
+    res.json(allData.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // get all ny state GeoJSON data
-app.get("/nystateGeoJSON", async(req,res) => {
-    try {
-      const allData = await pool.query(" \
+app.get("/nystateGeoJSON", async (req, res) => {
+  try {
+    const allData = await pool.query(
+      " \
         SELECT * \
         FROM ny_state_geojson \
-      ");
-      res.json(allData.rows);
-    } catch (err) {
-      console.error(err.message);
-    }
-})
+      "
+    );
+    res.json(allData.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
-  console.log('server has started on port 5000')
-})
+  console.log("server has started on port 5000");
+});
--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
-const pool = new Pool ({
-  user:'postgres',
-  password:'database1',
-  host:'localhost',
-  port:5432,
-  database:'cany'
-})
+const pool = new Pool({
+  user: "postgres",
+  password: "em718650",
+  host: "localhost",
+  port: 5432,
+  database: "uc",
+});
 
 module.exports = pool;
