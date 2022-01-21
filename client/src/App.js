3d3a53405303f37355241c60fef514f3dd1c068c
Evan Misshula <emisshula@jjay.cuny.edu>
2022-01-21 03:34:42 -0500

    cleans conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,69 @@
-import React from 'react';
-import ReactDropdown from 'react-dropdown';
-import { useState } from 'react';
+import React from "react";
+import ReactDropdown from "react-dropdown";
+import { useState } from "react";
 
 // underCustody imports
-import { useJSON, transformData } from './barchart/useData'
+import { useJSON, transformData } from "./barchart/useData";
 import { Chart } from "./barchart/bar";
 
 // table imports
-import { Table } from './table/table'
+import { Table } from "./table/table";
 
 // GeoJSON imports
-import { useGeoJSON } from './state_map/useGeoJSON'
+import { useGeoJSON } from "./state_map/useGeoJSON";
 import { MemoizedMap } from "./state_map/map";
 
 // css import
-import './App.css';
+import "./App.css";
 
 const App = () => {
-
   const rawData = useJSON();
   const mapData = useGeoJSON();
 
   // hooks
-  const [xAttribute, setXAttribute] = useState('sex'); // barchart x attribute
-  const [yAttribute, setYAttribute] = useState('amount'); // barchart y attribute
+  const [xAttribute, setXAttribute] = useState("sex"); // barchart x attribute
+  const [yAttribute, setYAttribute] = useState("amount"); // barchart y attribute
 
   if (!rawData || !mapData) {
     return <h2>Loading...</h2>;
   }
-
 
   // the data comes in ----------------------------------------------------------------------------
   // deal with undercustody data ------------------------------------------------------------------
   const barData = transformData(rawData, xAttribute);
   // map each column to { value: col, label: col } to feed into react Dropdown menu
   const xFields = Object.keys(rawData[0]).map((d) => ({
-    value: d, label: d
+    value: d,
+    label: d,
   }));
   const yFields = Object.keys(barData[0].value).map((d) => ({
-    value: d, label: d
+    value: d,
+    label: d,
   }));
-
 
   return (
     <>
-      <img src="https://static1.squarespace.com/static/5b2c07e2a9e02851fb387477/t/5c421dc203ce64393d395bb8/1616181909405/?format=1500w" />
+      <img
+        src="https://static1.squarespace.com/static/5b2c07e2a9e02851fb387477/t/5c421dc203ce64393d395bb8/1616181909405/?format=1500w"
+        alt="CANY Logo"
+      />
 
-      <hr/>
+      <hr />
       {/*barchart*/}
-      <div id='barchart'>
+      <div id="barchart">
         <h1> NYDOCCS Under Custody Data </h1>
-        <div id='barchart_menu' className="menu-container">
+        <div id="barchart_menu" className="menu-container">
           <span className="dropdown-label">X</span>
-            <ReactDropdown
+          <ReactDropdown
             options={xFields}
             value={xAttribute}
-            onChange={({ value, label }) =>
-              setXAttribute(value)
-            }
+            onChange={({ value, label }) => setXAttribute(value)}
           />
           <span className="dropdown-label">Y</span>
           <ReactDropdown
             options={yFields}
             value={yAttribute}
-            onChange={({ value, label }) =>
-              setYAttribute(value)
-            }
+            onChange={({ value, label }) => setYAttribute(value)}
           />
         </div>
         <Chart
@@ -84,15 +82,18 @@ const App = () => {
         totalPopulation={rawData.length}
       />
 
-      <hr/>
+      <hr />
 
       {/*ny_state_map*/}
-      <div id='ny_state_map'>
-        <h1 id='map' className='text-center'> New York State Map Data</h1>
-          <MemoizedMap data={mapData}/>
+      <div id="ny_state_map">
+        <h1 id="map" className="text-center">
+          {" "}
+          New York State Map Data
+        </h1>
+        <MemoizedMap data={mapData} />
       </div>
     </>
   );
-}
+};
 
 export default App;
--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "database1",
+  password: "em718650",
   host: "localhost",
   port: 5432,
-  database: "cany",
+  database: "uc",
 });
 
 module.exports = pool;
